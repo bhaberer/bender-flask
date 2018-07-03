2276013b05609cdd5ff266aa2a40226de69c50dc
Brian Haberer <bhaberer@gmail.com>
2018-07-03 13:00:23 -0700

    First bits

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import request
 import logging
 import time
 
+app = flask.Flask(__name__)
+
 @app.route('/')
 def success():
   return 'I am a success!', 200
+
+app.run(debug=True, host='0.0.0.0', port=80)
